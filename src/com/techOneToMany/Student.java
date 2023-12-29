01f649cff5b71c9a4197b06f965ec58c3f829f39
gadgeswapnil11 <gadgeswapnil11@gmail.com>
2023-12-29 18:45:11 +0530

    One To Many Mapping in Hibernate

---
+++ src/com/techOneToMany/Student.java
@@ -0,0 +1,5 @@
+package com.techOneToMany;
+
+public class Student {
+
+}
---
+++ src/com/techOneToMany/Test.java
@@ -0,0 +1,10 @@
+package com.techOneToMany;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
